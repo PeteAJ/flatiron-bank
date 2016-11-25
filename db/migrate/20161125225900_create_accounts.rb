1554d30fa1ea73b46d5fdbba17e7f5120ff1e9f5
PeteAJ <peter.jacobson@gmail.com>
2016-11-25 17:04:17 -0600

    columns to accounts migration

--- db/migrate/20161125225900_create_accounts.rb
+++ db/migrate/20161125225900_create_accounts.rb
@@ -1,6 +1,9 @@
 class CreateAccounts < ActiveRecord::Migration[5.0]
   def change
     create_table :accounts do |t|
+      t.string :name
+      t.integer :balance
+      t.boolean :overdraft_protection, :default => false
 
       t.timestamps
     end
