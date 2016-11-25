33079e8c13f6e45852d1390a60103829ca60ac69
PeteAJ <peter.jacobson@gmail.com>
2016-11-25 17:11:42 -0600

    clients migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125225900) do
+ActiveRecord::Schema.define(version: 20161125230852) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20161125225900) do
     t.boolean  "overdraft_protection", default: false
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
+  end
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
