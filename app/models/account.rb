dc9b94e1f4226da8abc8abe904e9b5d6b59c2441
PeteAJ <peter.jacobson@gmail.com>
2016-11-25 17:08:41 -0600

    accounts model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,12 @@
 class Account < ApplicationRecord
+  belongs_to :client
+  has_many :transactions
+
+
+def create_transaction(type,amount)
+  self.transactions.create(description: type, amount: amount)
+end
+
+
+
 end
