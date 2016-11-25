79e615c2b26e0124e6d997b60f3f90c02b32d919
PeteAJ <peter.jacobson@gmail.com>
2016-11-25 17:47:13 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :clients
+  root 'application#index', as: 'index'
+
+
+  resources :application
   resources :clients
   resources :accounts
+  resources :sessions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+get ‘/’, to: 'application#index'
 end
