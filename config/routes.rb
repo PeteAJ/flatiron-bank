821712d098e5c9a95a029a9a0dd0c7cefe013a32
PeteAJ <peter.jacobson@gmail.com>
2016-11-26 13:29:28 -0600

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
   root 'application#index', as: 'index'
-
-
-  resources :application
-  resources :clients
-  resources :accounts
-  resources :sessions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :accounts, :clients, :sessions
 end
