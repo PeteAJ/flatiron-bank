011dc8b706360645899e116902deed858c076b8f
PeteAJ <peter.jacobson@gmail.com>
2016-11-26 12:33:32 -0600

    port - sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,49 @@ require "./app/models/client"
 
 
 class SessionsController < ApplicationController
+
+  def signup
+    end
+
+  def registrations
+    if params[:name] == "" || params[:email] == "" || params[:password] == ""
+    else
+    @client = Client.new(params)
+    if @client.save
+    session[:client_id] = @client.id
+    else
+    flash[:notice] = "Please enter name, email and password."
+    end
+      end
+    end
+
+
+
+
+
+    def login
+    end
+
+    def sessions
+    client = Client.find_by_email(params[:email])
+    if client && client.authenticate(params[:password])
+    session[:client_id] = client.id
+    redirect to "/clients/#{client.id}"
+    else
+    flash[:notice] = "Please enter email and password."
+    redirect '/login'
+      end
+    end
+
+
+
+
+
+
+    def logout
+    logout
+    end
+
+    def client_index
+    end
 end
