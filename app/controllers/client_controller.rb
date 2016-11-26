71c52a29811317bad7d38ab9c0867e6b53661f3f
PeteAJ <peter.jacobson@gmail.com>
2016-11-26 12:30:42 -0600

    port - clients controller

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -7,12 +7,16 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.all
+    #@clients = Client.all
   end
 
   # GET /clients/1
   # GET /clients/1.json
   def show
+    @client = Client.find_by_id(params[:id])
+    if params[:account]
+    @account = Account.find_by_id(params[:account][:id])
+    end
   end
 
   # GET /clients/new
@@ -22,6 +26,7 @@ class ClientsController < ApplicationController
 
   # GET /clients/1/edit
   def edit
+      @client = Client.find_by_id(params[:id])
   end
 
   # POST /clients
@@ -29,40 +34,34 @@ class ClientsController < ApplicationController
   def create
     @client = Client.new(client_params)
 
-    respond_to do |format|
-      if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
-        format.json { render :show, status: :created, location: @client }
-      else
-        format.html { render :new }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
-    end
+    #respond_to do |format|
+    #  if @client.save
+    #    format.html { redirect_to @client, notice: 'Client was successfully created.' }
+    #    format.json { render :show, status: :created, location: @client }
+    #  else
+    #    format.html { render :new }
+    #    format.json { render json: @client.errors, status: :unprocessable_entity }
+    #  end
+    #end
   end
 
   # PATCH/PUT /clients/1
   # PATCH/PUT /clients/1.json
   def update
-    respond_to do |format|
-      if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-        format.json { render :show, status: :ok, location: @client }
-      else
-        format.html { render :edit }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
-    end
+    @client = Client.find_by_id(params[:id])
+    @client.email = params[:email]
+    @client.save
+    #respond_to do |format|
+    #  if @client.update(client_params)
+    #    format.html { redirect_to @client, notice: 'Client was successfully updated.' }
+    #    format.json { render :show, status: :ok, location: @client }
+    #  else
+    #    format.html { render :edit }
+    #    format.json { render json: @client.errors, status: :unprocessable_entity }
+    #  end
+    #end
   end
 
-  # DELETE /clients/1
-  # DELETE /clients/1.json
-  def destroy
-    @client.destroy
-    respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
