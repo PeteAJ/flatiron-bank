b00947dd1722cefe1b946297a533fa5573b9f835
PeteAJ <peter.jacobson@gmail.com>
2016-11-28 14:57:41 -0600

    revise edit forms with form_for plus routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
   root 'application#index', as: 'index'
-resources :accounts, :clients
+resources :accounts, only: [:index, :show, :new, :edit, :create]
+resources :clients, only: [:index, :show, :new, :edit, :create]
 
 get 'signup', to: 'sessions#signup'
 get 'login', to: 'sessions#login'
 
 post '/clients/:id', to: 'sessions#registrations', as: 'signed-up'
 post '/clients/:id', to: 'sessions#sessions', as: 'logged-in'
+
+patch 'accounts/:id', to: 'accounts#update'
+patch 'clients/:id', to: 'clients#update'
 end
