723c36eb83e996da477eb2513c5d2a3dd2e94c13
PeteAJ <peter.jacobson@gmail.com>
2016-11-30 15:09:58 -0500

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,16 +4,16 @@ class SessionsController < ApplicationController
     end
 
   def registrations
-    if params[:name] == "" || params[:email] == "" || params[:password] == ""
-      render "signup"
-    else
+    return redirect_to(controller: 'sessions', action: 'signup') if
+    params[:name].empty? || params[:email].empty? || params[:password].empty?
+
     @client = Client.new(params[:id])
     if @client.save
     session[:client_id] = @client.id
     redirect_to client_path(@client)
     else
     flash[:notice] = "Please enter name, email and password."
-      render "signup"
+    render "signup"
     end
       end
     end
@@ -26,10 +26,13 @@ class SessionsController < ApplicationController
     end
 
     def sessions
+    return redirect_to(controller: 'sessions', action: 'login') if
+    params[:email].empty? || params[:password].empty?
+
     client = Client.find_by_email(params[:email])
     if client && client.authenticate(params[:password])
     session[:client_id] = client.id
-    redirect_to "/client_path"
+    redirect_to client_path
     else
     flash[:notice] = "Please enter email and password."
     redirect_to '/login'
