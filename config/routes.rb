9f125c385d0f187fdeae2ef9aec8ae76347b8ca5
PeteAJ <peter.jacobson@gmail.com>
2016-12-13 18:24:57 -0500

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup'}, :controllers => { registrations: 'registrations', :omniauth_callbacks => "callbacks" }
-
+#devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup'}, :controllers => { registrations: 'registrations', :omniauth_callbacks => "callbacks" }
+devise_for :users, controllers: {
+  sessions: 'sessions/new'
+}
 
 # devise_scope :user do
 #   get "/" => "devise/sessions#new"
@@ -11,10 +13,12 @@ devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out =
 # devise_scope :user do
 #   root to: "users/sessions#new"
 # end
-
 resources :accounts, only: [:index, :show, :new, :edit, :create, :update]
 resources :clients, only: [:index, :show, :new, :edit, :create, :update]
 
+
 root to: "users/sessions#new"
+
+
 
 end
